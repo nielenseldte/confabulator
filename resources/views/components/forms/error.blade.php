13764eb5ff029fa9fee6de5d81fe3a5d76f2ffdd
Nielen Seldte <nielen@seldte.email>
2025-05-13 21:03:04 +0200

    regex patch for profanity filter

--- resources/views/components/forms/error.blade.php
+++ resources/views/components/forms/error.blade.php
@@ -1,6 +1,6 @@
 @props(['error' => false])
 
 @if ($error)
-    <p class="mt-2 text-sm text-red-600 bg-black px-2 py-2 border-t border-b border-red-600">ERROR: {{ $error }}
+    <p class="mt-2 text-sm text-red-600 bg-black px-2 py-2 border-t border-b border-red-600">OOPS! {{ $error }}
     </p>
 @endif
